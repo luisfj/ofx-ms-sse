3416028b9860b8228f7dcf8a7747b708aa0e4fa2
luis <luisfj_pr@hotmail.com>
2024-07-30 20:02:14 +0000

    Update for CI CD pass

--- src/test/java/dev/luisjohann/ofxmssse/OfxmssseApplicationTests.java
+++ src/test/java/dev/luisjohann/ofxmssse/OfxmssseApplicationTests.java
@@ -2,8 +2,10 @@ package dev.luisjohann.ofxmssse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class OfxmssseApplicationTests {
 
 	@Test
